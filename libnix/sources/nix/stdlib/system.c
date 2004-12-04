8acaa7445af020e89dd14992576730ac5cae609a
jshepher <>
2004-12-04 00:51:39 +0000

    Provide pre-V37 compatibility.

--- libnix/sources/nix/stdlib/system.c
+++ libnix/sources/nix/stdlib/system.c
@@ -1,12 +1,18 @@
 #include <dos/dos.h>
 #include <utility/tagitem.h>
 #include <proto/dos.h>
+#include <errno.h>
 
 int system(const char *string)
 { static struct TagItem notags[]={ { TAG_END,0 } };
 
-  if(!string)
-    return 1;
+  if(string == NULL) {
+    errno = EINVAL;
+    return -1;
+  }
+
+  if (((struct Library *)DOSBase)->lib_Version >= 36)
+    return SystemTagList((char *)string, notags);
   else
-    return SystemTagList((char *)string,notags); 
+    return (int)~Execute((STRPTR)string, 0l, Output());
 }
