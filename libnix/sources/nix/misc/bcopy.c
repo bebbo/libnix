7a5099b1d80b13fe971465de78da535902655c85
jshepher <>
2004-12-11 18:17:15 +0000

    Allow for overlapping regions.

--- libnix/sources/nix/misc/bcopy.c
+++ libnix/sources/nix/misc/bcopy.c
@@ -5,8 +5,17 @@ void bcopy(const void *s1,void *s2,size_t n)
 {
     unsigned char *ch1 = s1;
     unsigned char *ch2 = s2;
-    while (n > 0) {
-	*ch2++ = *ch1++;
-	n--;
+    if (ch1 < ch2) {
+	while (n > 0) {
+	    *ch2++ = *ch1++;
+	    n--;
+	}
+    } else {
+	ch1 += n;
+	ch2 += n;
+	while (n > 0) {
+	    *ch2-- = *ch1--;
+	    n--;
+	}
     }
 }
