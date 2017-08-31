297ab70f2c65c09b803edb5458ad340f4915b6a5
bebbo <bebbo@bejy.net>
2017-08-31 16:27:14 +0200

    use & instead of % if possible

--- sources/nix/stdlib/rand.c
+++ sources/nix/stdlib/rand.c
@@ -3,7 +3,11 @@
 static unsigned int a=1;
 
 int rand(void)
+#if (RAND_MAX==0x7fffffff)
+{ return (a=a*1103515245+12345)&RAND_MAX; }
+#else
 { return (a=a*1103515245+12345)%RAND_MAX; }
+#endif
 
 void srand(unsigned int seed)
 { a=seed; }
