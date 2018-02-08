eea3c3c2ef5b2230198d0ac34fb23145c6406205
bebbo <bebbo@bejy.net>
2018-02-08 12:44:21 +0100

    better code for strncmp

--- sources/nix/string/strncmp.c
+++ sources/nix/string/strncmp.c
@@ -1,5 +1,6 @@
 #include <string.h>
 
+#if 0
 int strncmp(const char *s1,const char *s2,size_t n)
 { unsigned char *p1=(unsigned char *)s1,*p2=(unsigned char *)s2;
   unsigned long r,c;
@@ -8,3 +9,26 @@ int strncmp(const char *s1,const char *s2,size_t n)
     do;while(r=*p1++,c=*p2++,!(r-=c) && (char)c && --n);
   return r;
 }
+#endif
+
+/*
+ * Yields better code.
+ */
+int strncmp(const char *p1,const char *p2, size_t n)
+{
+  for(;;) {
+	  if (n--) {
+		  char c = *p1++;
+		  if (c) {
+			if (c == *p2++)
+			  continue;
+		  } else
+			++p2;
+		  unsigned char a = *--p1;
+		  unsigned char b = *--p2;
+		  return (short)a - b;
+	  }
+	  return 0;
+	}
+}
+
