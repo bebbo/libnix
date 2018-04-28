aac08463c09c4c30a3896955d3e493a6af75592c
bebbo <bebbo@bejy.net>
2018-04-28 08:40:16 +0200

    refs #7: fixed inserting 0

--- sources/nix/stdio/vsnprintf.c
+++ sources/nix/stdio/vsnprintf.c
@@ -17,7 +17,9 @@ int vsnprintf(char *s,size_t size,const char *format,va_list args) {
 	buffer.flags=__SSTR|__SWR;
 	buffer.linebufsize=0;
 	retval=__vfprintf_total_size(&buffer,format,args);
-	s[size-1] = '\0';
-	
+	if (retval < size)
+		s[retval] = 0;
+	else if (size > 0)
+		s[size - 1] = 0;
 	return retval;
 }
