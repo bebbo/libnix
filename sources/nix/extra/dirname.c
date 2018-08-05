869408aca5cbb4b7b3b3072838460c3eae3168e2
bebbo <bebbo@bejy.net>
2018-08-05 21:19:54 +0200

    add dirname

---
+++ sources/nix/extra/dirname.c
@@ -0,0 +1,24 @@
+#include <proto/dos.h>
+
+char *dirname(char *path) {
+	if (!path || !*path)
+		return ".";
+
+	char *p = path;
+	while (*p) {
+		++p;
+	}
+	while (--p > path && *p == '/') {
+		*p = 0;
+	}
+	while (--p >= path && *p != '/') {
+		*p = 0;
+	}
+	if (p > path && *p == '/')
+		*p = 0;
+
+	if (!*path || 0 == strcmp("..", path))
+		return ".";
+
+	return path;
+}
