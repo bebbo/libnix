866749861bdb0920470daceddb57c8b4f18ae552
bebbo <bebbo@bejy.net>
2019-04-18 15:41:25 +0200

    shorted code with -Os

--- sources/nix/misc/__initlibraries.c
+++ sources/nix/misc/__initlibraries.c
@@ -4,52 +4,41 @@
 #include <string.h>
 
 extern struct lib /* These are the elements pointed to by __LIB_LIST__ */
-{ struct Library *base;
-  char *name;
-} *__LIB_LIST__[];
+{
+	struct Library *base;
+	char *name;
+}*__LIB_LIST__[];
 
 extern void exit(int returncode);
 
-static void __openliberror(ULONG,char const *);
 
-static void __openliberror(ULONG version, char const * ln)
-{
-  char buf[60];
-  strcpy(buf, "can't load ");
-  strcat(buf, ln);
-  strcat(buf, "\n");
-  Write(Output(), buf, strlen(buf));
-  exit(20);
+static __attribute__((noreturn)) void __openliberror(ULONG version, char const * ln) {
+	char buf[60];
+	strcpy(buf, ln);
+	strcat(buf, " failed\n");
+	Write(Output(), buf, strlen(buf));
+	exit(20);
 }
 
-void __initlibraries(void)
-{
-  struct lib **list=__LIB_LIST__;
-  ULONG numbases=(ULONG)*list++;
-
-  if(numbases)
-    do {
-      struct lib *l=*list++;
-      if((l->base=OpenLibrary(l->name,0))==NULL)
-        __openliberror(0,l->name);
-    } while(--numbases);
+void __initlibraries(void) {
+	struct lib **list = __LIB_LIST__ + 1;
+	struct lib * l;
+	while ((l = *list++)) {
+		if ((l->base = OpenLibrary(l->name, 0)) == NULL)
+			__openliberror(0, l->name);
+	}
 }
 
-void __exitlibraries(void)
-{
-  struct lib **list=__LIB_LIST__;
-  ULONG numbases=(ULONG)*list++;
-
-  if(numbases)
-    do {
-      struct lib *l=*list++;
-      struct Library *lb=l->base;
-      if(lb!=NULL) {
-        /* l->base=NULL; */
-        CloseLibrary(lb);
-      }
-    } while(--numbases);
+void __exitlibraries(void) {
+	struct lib **list = __LIB_LIST__ + 1;
+	struct lib * l;
+	while ((l = *list++)) {
+		struct Library *lb = l->base;
+		if (lb != NULL) {
+			CloseLibrary(lb);
+		}
+	}
 }
 
-ADD2INIT(__initlibraries,-60);
-ADD2EXIT(__exitlibraries,-60);
+ADD2INIT(__initlibraries, -60);
+ADD2EXIT(__exitlibraries, -60);
