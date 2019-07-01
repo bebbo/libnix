43ac30f5b72e377f1f4de2ee0e85d8796c95f601
bebbo <bebbo@bejy.net>
2019-07-01 09:29:41 +0200

    refs #15: add also a __fpucheck() method which gets linked if one of 68881, 68040, 68060 or 68080 is specified.

--- sources/nix/misc/__fpucheck.c
+++ sources/nix/misc/__fpucheck.c
@@ -7,26 +7,26 @@ asm(
 "	.text;"
 "	.even;"
 
-"	.globl	___cpucheck;"
+"	.globl	___fpucheck;"
 "___cpucheck:;"
 "	movel	"A4(_SysBase)",a1;"
 "	movew	a1@(0x128:W),d0;"
-"	btst	#1,d0;"
+"	btst	#4,d0;"
 "	jeq	error;"
 "	rts;"
 "error:"
 
 "   move.l  a1,a6;"
-"	move.l	#0x68020,d7;"
+"	move.l	#0x68881,d7;"
 "   jsr -0x6c(a6);"
 "	pea	20:W;"
 "	jsr	_exit;"		/* dito */
 );
 
-ADD2INIT(__cpucheck,-80); /* Highest priority */
+ADD2INIT(__fpucheck,-80); /* Highest priority */
 
 #else
 
-void __cpucheck(){}
+void __fpucheck(){}
 
 #endif
