0623ee66fc2e13fe467010e4c76fc03c023f2ff3
githubaf <alexander.fritsch@adtran.com>
2019-07-11 12:08:46 +0200

    enhanced test -> do something more than just a printf. Real pi calculation now

--- libnix_020_881_test/cpu_test.c
+++ libnix_020_881_test/cpu_test.c
@@ -3,11 +3,25 @@
 */
 
 #include <stdio.h>
-#include <math.h>
+
+// taken from https://stackoverflow.com/questions/32672693/calculating-pi-with-taylor-method-c
+
+
+double pi(int n) {
+    double sum = 0.0;
+    int sign = 1;
+    int i;
+    for (i = 0; i < n; ++i) {
+        sum += sign/(2.0*i+1.0);
+        sign *= -1;
+    }
+    return 4.0*sum;
+}
+
 
 int main(void)
 {
-        printf("Hello world, Pi=%f\n",M_PI);
+        printf("Hello world, Pi=%1.2f\n",pi(1000));
 
 #ifdef __HAVE_68881__ 
 	printf("___HAVE_68881____defined...\n");
