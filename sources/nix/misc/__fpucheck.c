95fa49766a2dce0165a9fe70f65e333ec85b50c2
githubaf <alexander.fritsch@adtran.com>
2020-01-16 09:39:29 +0000

    Autorequest() instead of Alert() for Need-fpu message

--- sources/nix/misc/__fpucheck.c
+++ sources/nix/misc/__fpucheck.c
@@ -2,25 +2,25 @@
 #include "stabs.h"
 
 #if defined(mc68020)
+const char errtext2[]="Need some FPU to run";
 
 asm(
-"	.text;"
-"	.even;"
+"              .text;"
+"              .even;"
 
-"	.globl	___fpucheck;"
+"              .globl     ___fpucheck;"
 "___fpucheck:;"
-"	movel	"A4(_SysBase)",a1;"
-"	movew	a1@(0x128:W),d0;"
-"	btst	#4,d0;"
-"	jeq	error;"
-"	rts;"
+"              movel   "A4(_SysBase)",a1;"
+"              movew a1@(0x128:W),d0;"
+"              lea          _errtext2,a0;"
+"              btst        #4,d0;"
+"              jeq         error;"
+"              rts;"
 "error:"
-
-"   move.l  a1,a6;"
-"	move.l	#0x68881,d7;"
-"   jsr -0x6c(a6);"
-"	pea	20:W;"
-"	jsr	_exit;"		/* dito */
+"              movel   a0,sp@-;"
+"              jsr           ___myrequest;" /* jbsr translates to jra which doesn't work on 68000 */
+"              pea        20:W;"
+"              jsr           _exit;"               
 );
 
 ADD2INIT(__fpucheck,-80); /* Highest priority */
