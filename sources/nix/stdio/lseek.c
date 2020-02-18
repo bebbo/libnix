dea42169353db24b1ad4387830d8fd99363e5abc
bebbo <bebbo@bejy.net>
2020-02-18 18:14:01 +0100

    add strtok_r(...)

--- sources/nix/stdio/lseek.c
+++ sources/nix/stdio/lseek.c
@@ -18,38 +18,50 @@ __seterrno(void);
 off_t lseek(int d, off_t offset, int whence) {
 	StdFileDes *sfd = _lx_fhfromfd(d);
 
-	__chkabort();
 	if (sfd) {
 		off_t r;
 		int file = sfd->lx_fh;
-		// to support a seek behind file end to extend the file it's necessary to know the resulting position.
-		off_t abs_pos = offset;
 
-		// any write mode determine the resulting seek position
-		if (sfd->lx_oflags) {
-			if (whence == SEEK_CUR)
-				abs_pos += Seek(file, 0, OFFSET_CURRENT);
-			else if (whence == SEEK_END && whence > 0) {
-				Seek(file, 0, OFFSET_END);
-				abs_pos += Seek(file, 0, OFFSET_END);
+		// seek ...
+		r = Seek(file, offset, whence == SEEK_SET ? OFFSET_BEGINNING : whence == SEEK_END ? OFFSET_END : OFFSET_CURRENT);
+
+		// seek failed - check if it would extend the file
+		if (r == EOF && sfd->lx_oflags) {
+			// to support a seek behind file end to extend the file it's necessary to know the resulting position.
+			off_t abs_pos = offset;
+
+			// determine the resulting seek position
+			if (whence == SEEK_CUR) {
+				r = Seek(file, 0, OFFSET_CURRENT);
+				abs_pos += r;
+			} else if (whence == SEEK_END) {
+				if (whence <= 0)
+					r = EOF;
+				else {
+					Seek(file, 0, OFFSET_END);
+					r = Seek(file, 0, OFFSET_END);
+					abs_pos += r;
+				}
+			} else { // SEEK_SET
+				r = 0;
 			}
-		}
 
-		// seek without checking ...
-		Seek(file, offset, whence == SEEK_SET ? OFFSET_BEGINNING : whence == SEEK_END ? OFFSET_END : OFFSET_CURRENT);
-		r = Seek(file, 0, OFFSET_CURRENT);
+			// if not at end yet - seek
+			if (r != EOF && whence != SEEK_END) {
+				Seek(file, 0, OFFSET_END);
+				r = Seek(file, 0, OFFSET_END);
+			}
 
-		// if the file is too small
-		if (r != EOF && abs_pos > r && sfd->lx_oflags) {
-			// and extend the file to reach that offset.
-			static char * tmp;
-			unsigned sz = 0x1000;
-			if (!tmp)
-				tmp = malloc(sz);
+			// still ok?
+			if (r != EOF && abs_pos > r) {
+				// if the file is too small
+				// extend the file to reach that offset.
+				static char *tmp;
+				unsigned sz = 0x1000;
+				if (!tmp)
+					tmp = malloc(sz);
 
-			// note that SetFileSize does not always work
-			Seek(file, 0, OFFSET_END);
-			if ((r = Seek(file, 0, OFFSET_END)) != EOF) {
+				// note that SetFileSize does not always work
 				unsigned diff = abs_pos - r;
 				while (diff) {
 					unsigned chunk = diff > sz ? sz : diff;
