92fbb14ddd7150d09ffd9a7a1946e2a20b32756b
bebbo <bebbo@bejy.net>
2020-03-09 18:32:52 +0100

    refs #19: there is now a check for 68881 if a 68881 variant is linked

--- sources/nix/misc/__cpucheck.c
+++ sources/nix/misc/__cpucheck.c
@@ -9,15 +9,25 @@ asm(
 
 "	.globl	___cpucheck;"
 "___cpucheck:;"
+"	move.l	#0x68020,d1;"
 "	movel	"A4(_SysBase)",a1;"
 "	movew	a1@(0x128:W),d0;"
 "	btst	#1,d0;"
 "	jeq	error;"
+);
+#ifdef __HAVE_68881__
+asm(
+"	move.l	#0x68881,d1;"
+"	btst	#4,d0;"
+"	jeq	error;"
+);
+#endif
+asm(
 "	rts;"
 "error:"
 
 "   move.l  a1,a6;"
-"	move.l	#0x68020,d7;"
+"	move.l	d1,d7;"
 "   jsr -0x6c(a6);"
 "	pea	20:W;"
 "	jsr	_exit;"		/* dito */
