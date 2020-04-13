18f0affd7c136c272bc10e60a84a0c2305590633
bebbo <bebbo@bejy.net>
2020-04-13 21:19:56 +0200

    refs https://github.com/bebbo/newlib-cygwin/issues/13: make timeval in devices/timer.h identical v4

--- sources/nix/extra/nanosleep.c
+++ sources/nix/extra/nanosleep.c
@@ -1,3 +1,4 @@
+#include <sys/_timeval.h>
 #include <devices/timer.h>
 #include <unistd.h>
 
@@ -6,12 +7,12 @@ extern void dotimer(ULONG,struct timeval *);
 int nanosleep(const struct timespec *req, struct timespec *rem)
 { struct timeval tv;
 
-  tv.tv_secs = req->tv_secs;
+  tv.tv_secs = req->tv_sec;
   tv.tv_micro = req->tv_nsec / 1000;
 
   dotimer(TR_ADDREQUEST,&tv);
   if (rem) {
-	  rem->tv_secs = 0;
+	  rem->tv_sec = 0;
 	  rem->tv_nsec = 0;
   }
   return 0;
