a430900e4e079d4f6e895c69ec4490546b36c310
bebbo <bebbo@bejy.net>
2020-10-19 20:34:11 +0200

    add timeradd and timersub
    see also  https://github.com/bebbo/amiga-gcc/issues/192

---
+++ sources/nix/time/timeradd.c
@@ -0,0 +1,19 @@
+struct timeval {
+    union {
+        long          tv_sec;         /* seconds */
+        long          tv_secs;
+    };
+    union {
+        long     tv_usec;        /* and microseconds */
+        long     tv_micro;
+    };
+};
+
+void timersub(struct timeval *a, struct timeval *b, struct timeval *res) {
+	res->tv_sec = a->tv_sec - b->tv_sec;
+	res->tv_usec = a->tv_usec - b->tv_usec;
+	if (res->tv_usec >= 1000000) {
+		++res->tv_sec;
+		res->tv_usec -= 1000000;
+	}
+}
