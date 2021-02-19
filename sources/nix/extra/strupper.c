bebbo/libnix

commit 7da928e6ae648a015013fc8f1f1eecfbf613940c

remove ALIAS in favor of asm labels

sources/nix/extra/strupper.c
@@ -1,6 +1,6 @@
 #include <ctype.h>
-#include "stabs.h"
 
+asm("_strupr: .global strupr");
 char *strupper(char *s)
 { unsigned char *s1;
 
@@ -12,5 +12,3 @@ char *strupper(char *s)
   }
   return s;
 }
-
-ALIAS(strupr,strupper);

sources/nix13/extra/strnicmp.c
@@ -1,7 +1,7 @@
 #include <ctype.h>
 #include <string.h>
-#include "stabs.h"
 
+asm("_strncasecmp: .global strncasecmp");
 int strnicmp(const char *s1,const char *s2,size_t len)
 { unsigned char c1,c2;
   int r;
@@ -23,5 +23,3 @@ int strnicmp(const char *s1,const char *s2,size_t len)
     }
   return r;
 }
-
-ALIAS(strncasecmp,strnicmp);

sources/nix20/extra/strnicmp.c
@@ -1,12 +1,8 @@
-#ifndef __KICK13__
 #include <string.h>
 #include <proto/utility.h>
-#include "stabs.h"
 
+asm("_strncasecmp: .global _strncasecmp");
 int strnicmp(const char *s1,const char *s2,size_t len)
 {
   return Strnicmp((STRPTR)s1,(STRPTR)s2,(LONG)len);
 }
-
-ALIAS(strncasecmp,strnicmp);
-#endif

sources/nix/extra/bcmp.c
@@ -1,3 +1,0 @@
-#include "stabs.h"
-
-ALIAS(bcmp,memcmp);

sources/nix/extra/getppid.c
@@ -1,12 +1,10 @@
 #include <unistd.h>
-#include "stabs.h"
 
+asm("_getuid: .global _getuid");
+asm("_geteuid: .global _geteuid");
+asm("_getgid: .global _getgid");
+asm("_getegid: .global _getegid");
 pid_t getppid(void)
 {
   return 0; /* init ;-) */
 }
-
-ALIAS(getuid, getppid);
-ALIAS(geteuid, getppid);
-ALIAS(getgid, getppid);
-ALIAS(getegid, getppid);

sources/nix/extra/getruntime.c
@@ -6,11 +6,10 @@ process started.
 
 #include <stdlib.h>
 #include <time.h>
-#include <stabs.h>
 
+asm("_getruntime: .global _getruntime");
 long get_run_time (void)
 {
   /* Fall back on clock since it's correctly implemented. */
   return clock() * 10000;
 }
-ALIAS(getruntime,get_run_time);

sources/nix/extra/rmdir.c
@@ -1,3 +1,0 @@
-#include "stabs.h"
-
-ALIAS(rmdir,remove);

sources/nix/extra/stat.c
@@ -3,12 +3,12 @@
 #include <proto/exec.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include "stabs.h"
 
 extern int __stat(struct stat *buf,struct FileInfoBlock *fib);
 extern void __seterrno(void);
 extern char *__amigapath(const char *path);
 
+asm("_lstat: .global _lstat");
 int stat(const char *name,struct stat *buf)
 { APTR oldwin,*wptr=&((struct Process *)FindTask(NULL))->pr_WindowPtr;
   struct FileInfoBlock *fib;
@@ -44,5 +44,3 @@ int stat(const char *name,struct stat *buf)
 
   return ret;
 }
-
-ALIAS(lstat,stat);

commit ea417c491e7b935375692c943f3eb54985d4183f

Fix typos in function aliases
A typo in 7da928e broke strncasecmp when compiling with -mcrt=nix13 due
to a missing underscore.

sources/nix/extra/strupper.c
@@ -1,6 +1,6 @@
 #include <ctype.h>
 
-asm("_strupr: .global strupr");
+asm("_strupr: .global _strupr");
 char *strupper(char *s)
 { unsigned char *s1;
 

sources/nix13/extra/strnicmp.c
@@ -1,7 +1,7 @@
 #include <ctype.h>
 #include <string.h>
 
-asm("_strncasecmp: .global strncasecmp");
+asm("_strncasecmp: .global _strncasecmp");
 int strnicmp(const char *s1,const char *s2,size_t len)
 { unsigned char c1,c2;
   int r;