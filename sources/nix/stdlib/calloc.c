4a87ee1fbb8440769772ea986f969c956aaf9ed2
bebbo <bebbo@bejy.net>
2022-03-06 13:57:21 +0100

    callo allocates at least 1 byte...

--- sources/nix/stdlib/calloc.c
+++ sources/nix/stdlib/calloc.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 
-void *calloc(size_t nmemb,size_t size)
-{
-  size_t l=(nmemb*size+(sizeof(size_t)-1))&~(sizeof(size_t)-1);
-  void *b=malloc(l);
-  if(b!=NULL)
-	  memset(b, 0, l);
-  return b;
+void* calloc(size_t nmemb, size_t size) {
+	size_t l = nmemb * size;
+	if (!l)
+		l = 1;
+	void *b = malloc(l);
+	if (b != NULL)
+		memset(b, 0, l);
+	return b;
 }
