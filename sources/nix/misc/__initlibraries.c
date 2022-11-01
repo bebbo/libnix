437d423a5c276302a49e4cce2dee2056c7ab1dc7
bebbo <bebbo@bejy.net>
2022-11-01 08:55:52 +0100

    avoid generated asm code with negative offsets, since LIB_LIST ist first

--- sources/nix/misc/__initlibraries.c
+++ sources/nix/misc/__initlibraries.c
@@ -3,48 +3,46 @@
 #include "stabs.h"
 #include <string.h>
 
-extern struct lib /* These are the elements pointed to by __LIB_LIST__ */
-{
-	struct Library *base;
-	char const *name;
-};
-
 extern long __LIB_LIST__;
 
 extern __attribute__((noreturn)) void exit(int returncode);
 
+#pragma GCC push_options
+#pragma GCC optimize ("-O2")
 
-static __attribute__((noreturn)) void __openliberror() {
-	char buf[60];
+static __attribute__((noreturn)) void __openliberror(char const * name) {
+	char buf[80];
 
-	struct lib * l = (struct lib *)(&__LIB_LIST__ + 1);
-	while (l->base) ++l;
-	strcpy(buf, l->name);
+	strcpy(buf, name);
 	strcat(buf, " failed to load\n");
 	Write(Output(), buf, strlen(buf));
 	exit(20);
 }
 
 void __initlibraries(void) {
-	struct lib * l = (struct lib *)(&__LIB_LIST__ + 1);
-	short err = 0;
-	while (l->base) {
-		if (!(l->base = strstr(l->name, ".resource") ? OpenResource(l->name) : OldOpenLibrary(l->name)))
-		err = 1;
-		++l;
+	long * l = &__LIB_LIST__ + 1;
+	while (*l) {
+		long * base = l++;
+		char const * const name = *(char **) (l++);
+		*base = (long)(strstr(name, ".resource") ? OpenResource(name) : OldOpenLibrary(name));
+		if (!*base) {
+ 		  __openliberror(name);
+ 		  break;
+		}
 	}
-	if (err)
-		__openliberror();
 }
 
 void __exitlibraries(void) {
-	struct lib * l = (struct lib *)(&__LIB_LIST__ + 1);
-	while (l->base) {
-		if (!strstr(l->name, ".resource"))
-			CloseLibrary(l->base);
-		++l;
+	long * l = &__LIB_LIST__ + 1;
+	while (*l) {
+		long * base = l++;
+		char const * const name = *(char **) (l++);
+		if (!strstr(name, ".resource"))
+			CloseLibrary((struct Library *)*base);
 	}
 }
+
+#pragma GCC pop_options
 
 ADD2INIT(__initlibraries, -79);
 ADD2EXIT(__exitlibraries, -79);
