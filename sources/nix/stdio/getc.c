3d5ca3a40151d17ad2a9c58d5c7c625ea4986cbb
bebbo <bebbo@bejy.net>
2022-11-07 12:13:36 +0100

    refs #61: undo change in __srget
    temporarily limit buffer to 1 in getc

--- sources/nix/stdio/getc.c
+++ sources/nix/stdio/getc.c
@@ -5,8 +5,13 @@ int getc(FILE *stream) {
 	__STDIO_LOCK(stream);
 	if (--stream->_r >= 0)
 		r = *stream->_p++;
-	else
+	else {
+		// limit buffer size to 1 to read only one character.
+		int sz = stream->_bf._size;
+		stream->_bf._size = 1;
 		r = __srget(stream);
+		stream->_bf._size = sz;
+	}
 	__STDIO_UNLOCK(stream);
 	return r;
 }
