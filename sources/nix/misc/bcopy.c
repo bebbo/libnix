4d476c8cb907932e92302cfd1d502f4c5df327c2
bebbo <bebbo@bejy.net>
2023-01-05 09:01:58 +0100

    tune memset and bcopy

--- sources/nix/misc/bcopy.c
+++ sources/nix/misc/bcopy.c
@@ -1,90 +1,88 @@
 #include <stdlib.h>
 
+#define NNN 16
+
 /* This is a _fast_ block move routine! */
-typedef union {
-	void *v;
-	char *c;
-	short *s;
-	long *l;
-} val;
 void bcopy(const void *s1, void *s2, size_t n) {
 	size_t m;
-	val v1, v2;
+	register char const * from asm("a1") = (char const *)s1;
+    register char * to asm("a0") = (char *) s2;
 	if (!n)
 		return;
 
-	v1.v = (void *)s1;
-	v2.v = s2;
-
 	if (s2 < s1) {
-		if (n > 15) {
-#if defined(__mc68000) || defined(__mc68010)
-			if ( (((char)(long)s1) ^ ((char)(long)s2)) & 1 == 0) {
+		if (n > NNN) {
+#if !defined(__mc68020) && !defined(__mc68030) && !defined(__mc68040) && !defined(__mc68060) && !defined(__mc68080)
+			if (((((char)(long)from) ^ ((char)(long)to))) % 2 == 0) {
 #endif
-			if (((char)(long) v1.c) & 1) {
-				*v2.c++ = *v1.c++;
+			if (((long) to) & 1) {
+				asm volatile("move.b (%0)+,(%1)+" : "=r" (to), "=r" (from): "r" (to), "r" (from));
 				n--;
 			}
-			if (((char)(long) v1.c) & 2) {
-				*v2.s++ = *v1.s++;
+			if (((long) to) & 2) {
+				asm volatile("move.w (%0)+,(%1)+" : "+rm" (to), "+rm" (from): "r" (to), "r" (from));
 				n -= 2;
 			}
 			for (m = n / (8 * sizeof(long)); m; --m) {
-				*v2.l++ = *v1.l++;
-				*v2.l++ = *v1.l++;
-				*v2.l++ = *v1.l++;
-				*v2.l++ = *v1.l++;
-				*v2.l++ = *v1.l++;
-				*v2.l++ = *v1.l++;
-				*v2.l++ = *v1.l++;
-				*v2.l++ = *v1.l++;
+				asm volatile("move.l (%0)+,(%1)+" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+				asm volatile("move.l (%0)+,(%1)+" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+				asm volatile("move.l (%0)+,(%1)+" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+				asm volatile("move.l (%0)+,(%1)+" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+				asm volatile("move.l (%0)+,(%1)+" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+				asm volatile("move.l (%0)+,(%1)+" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+				asm volatile("move.l (%0)+,(%1)+" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+				asm volatile("move.l (%0)+,(%1)+" : "=r" (to), "=r" (from): "r" (to), "r" (from));
 			}
 			n &= 8 * sizeof(long) - 1;
 			for (m = n / sizeof(long); m; --m)
-				*v2.l++ = *v1.l++;
+				asm volatile("move.l (%0)+,(%1)+" : "=r" (to), "=r" (from): "r" (to), "r" (from));
 			n &= sizeof(long) - 1;
 			if (!n)
 				return;
 		}
-#if defined(__mc68000) || defined(__mc68010)
+#if !defined(__mc68020) && !defined(__mc68030) && !defined(__mc68040) && !defined(__mc68060) && !defined(__mc68080)
 		}
 #endif
-		while (*v2.c++ = *v1.c++, --n){}
+		while (n--){
+			asm volatile("move.b (%0)+,(%1)+" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+        }
 	} else {
-		v1.c += n;
-		v2.c += n;
-#if defined(__mc68000) || defined(__mc68010)
-			if ( (((char)s1) ^ ((char)s2)) & 1 == 0) {
+		from += n;
+		to += n;
+#if !defined(__mc68020) && !defined(__mc68030) && !defined(__mc68040) && !defined(__mc68060) && !defined(__mc68080)
+		if (((((char)(long)from) ^ ((char)(long)to))) % 2 == 0) {
 #endif
-		if (n > 15) {
-			if (((char)(long) v1.c) & 1) {
-				*--v2.c = *--v1.c;
+		if (n > NNN) {
+			if (((long) to) & 1) {
+				asm volatile("move.b -(%0),-(%1)" : "=r" (to), "=r" (from): "r" (to), "r" (from));
 				n--;
 			}
-			if (((char)(long) v1.c) & 2) {
-				*--v2.s = *--v1.s;
+			if (((long) to) & 2) {
+				asm volatile("move.w -(%0),-(%1)" : "=r" (to), "=r" (from): "r" (to), "r" (from));
 				n -= 2;
 			}
 			for (m = n / (8 * sizeof(long)); m; --m) {
-				*--v2.l = *--v1.l;
-				*--v2.l = *--v1.l;
-				*--v2.l = *--v1.l;
-				*--v2.l = *--v1.l;
-				*--v2.l = *--v1.l;
-				*--v2.l = *--v1.l;
-				*--v2.l = *--v1.l;
-				*--v2.l = *--v1.l;
+				asm volatile("move.l -(%0),-(%1)" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+				asm volatile("move.l -(%0),-(%1)" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+				asm volatile("move.l -(%0),-(%1)" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+				asm volatile("move.l -(%0),-(%1)" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+				asm volatile("move.l -(%0),-(%1)" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+				asm volatile("move.l -(%0),-(%1)" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+				asm volatile("move.l -(%0),-(%1)" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+				asm volatile("move.l -(%0),-(%1)" : "=r" (to), "=r" (from): "r" (to), "r" (from));
 			}
 			n &= 8 * sizeof(long) - 1;
 			for (m = n / sizeof(long); m; --m)
-				*--v2.l = *--v1.l;
+				asm volatile("move.l -(%0),-(%1)" : "=r" (to), "=r" (from): "r" (to), "r" (from));
 			n &= sizeof(long) - 1;
 			if (!n)
 				return;
 		}
-#if defined(__mc68000) || defined(__mc68010)
+#if !defined(__mc68020) && !defined(__mc68030) && !defined(__mc68040) && !defined(__mc68060) && !defined(__mc68080)
 		}
 #endif
-		while (*--v2.c = *--v1.c, --n){}
+		while (n--){
+            asm volatile("move.b -(%0),-(%1)" : "=r" (to), "=r" (from): "r" (to), "r" (from));
+        }
 	}
 }
--- sources/nix/string/memset.c
+++ sources/nix/string/memset.c
@@ -8,77 +8,48 @@ union V {
 };
 
 void *memset(void *s, int _c, size_t n) {
-	register unsigned c __asm("d2") = _c;
-	if (n < 8) {
-	    char * p = (char *) s;
-		if (n) {
-			*p++ = c;
-			if (--n) {
-				*p++ = c;
-				if (--n) {
-					*p++ = c;
-					if (--n) {
-						*p++ = c;
-						if (--n) {
-							*p++ = c;
-							if (--n) {
-								*p++ = c;
-								if (--n) {
-									*p++ = c;
-									if (--n) {
-										*p = c;
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		return s;
-	}
+	register unsigned c asm ("d2") = _c;
+	register char * p asm ("a0") = n + (char *) s;
 
-	{
-	    register char * p __asm("a0") = n + (char *) s;
-
-		c = _c & 0xff;
+	if (n > 18) {
+		c &= 0xff;
 		c |= c << 8;
 		c |= c << 16;
 
 		if ((long) p & 1) {
-			__asm("move.b d2,-(a0)" :: "r" (c, p));
+			asm volatile("move.b d2,-(a0)" :: "r" (c, p));
 			--n;
 		}
 		if ((long) p & 2) {
-			__asm("move.w d2,-(a0)" :: "r" (c,p));
-			__asm("subq #2,d1");
+			asm volatile("move.w d2,-(a0)" :: "r" (c,p));
+			asm volatile("subq #2,d1");
 		}
 		size_t m  = n / (8 * sizeof(long));
 		if (m) {
-			__asm("move.l d2,a6" ::"r" (c): "a6");
-			__asm("move.l d2,d5" ::"r" (c): "d5");
-			__asm("move.l d2,d6" ::"r" (c): "d6");
-			__asm("move.l d2,d7" ::"r" (c): "d7");
-			__asm("move.l d2,a1" ::"r" (c): "a1");
-			__asm("move.l d2,a2" ::"r" (c): "a2");
-			__asm("move.l d2,a3" ::"r" (c): "a3");
+			asm volatile("move.l d2,a6" ::"r" (c): "a6");
+			asm volatile("move.l d2,d5" ::"r" (c): "d5");
+			asm volatile("move.l d2,d6" ::"r" (c): "d6");
+			asm volatile("move.l d2,d7" ::"r" (c): "d7");
+			asm volatile("move.l d2,a1" ::"r" (c): "a1");
+			asm volatile("move.l d2,a2" ::"r" (c): "a2");
+			asm volatile("move.l d2,a3" ::"r" (c): "a3");
 			while (m--) {
-				__asm("movem.l d2/d5/d6/d7/a1/a2/a3/a6,-(a0)" ::"r" (c, p));
+				asm volatile("movem.l d2/d5/d6/d7/a1/a2/a3/a6,-(a0)" ::"r" (c, p));
 			}
 			n &= (8 * sizeof(long) - 1);
 		}
-        {
-		    size_t k = n >> 2;
+		{
+			size_t k = n >> 2;
 			for (; k; --k) {
-				__asm("move.l d2,-(a0)" :: "r" (c, p));
+				asm volatile("move.l d2,-(a0)" :: "r" (c, p));
 			}
 			n &= sizeof(long) - 1;
 		}
-
-		while(n--) {
-			__asm("move.b d2,-(a0)" :: "r" (c, p));
-		}
-
-		return s;
 	}
+
+	while(n--) {
+		asm volatile("move.b d2,-(a0)" : "=r" (p): "r" (c, p));
+	}
+
+	return p;
 }
