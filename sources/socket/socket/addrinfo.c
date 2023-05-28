de5b5dd0459d84977035bf5ffe99be1e0778c2f4
bebbo <bebbo@bejy.net>
2023-05-28 12:24:44 +0200

    implement without asm

--- sources/socket/socket/addrinfo.c
+++ sources/socket/socket/addrinfo.c
@@ -1,27 +1,32 @@
 #include "socket.h"
-#include <proto/bsdsocket.h>
 
-asm("_freeaddrinfo: .global _freeaddrinfo");
-void __freeaddrinfo(struct addrinfo *ai) {
-	freeaddrinfo(ai);
+#define freeaddrinfo __freeaddrinfo
+#define getaddrinfo __getaddrinfo
+#define getnameinfo __getnameinfo
+#define gai_strerror __gai_strerror
+#include <proto/bsdsocket.h>
+#undef freeaddrinfo
+#undef getaddrinfo
+#undef getnameinfo
+#undef gai_strerror
+
+void freeaddrinfo(struct addrinfo *ai) {
+	__freeaddrinfo(ai);
 }
 
-asm("_getaddrinfo: .global _getaddrinfo");
-int __getaddrinfo(const char *restrict nodename,
+int getaddrinfo(const char *restrict nodename,
        const char *restrict servname,
        const struct addrinfo *restrict hints,
        struct addrinfo **restrict res) {
-	return getaddrinfo(nodename, servname, hints, res);
+	return __getaddrinfo(nodename, servname, hints, res);
 }
 
-asm("_getnameinfo: .global _getnameinfo");
-int __getnameinfo(const struct sockaddr *addr, socklen_t addrlen,
+int getnameinfo(const struct sockaddr *addr, socklen_t addrlen,
                 char *host, socklen_t hostlen,
                 char *serv, socklen_t servlen, int flags) {
-	return getnameinfo(addr, addrlen, (STRPTR)host, hostlen, (STRPTR)serv, servlen, flags);
+	return __getnameinfo(addr, addrlen, (STRPTR)host, hostlen, (STRPTR)serv, servlen, flags);
 }
 
-asm("_gai_strerror: .global _gai_strerror");
-const char * __gai_strerror(int ecode) {
-	return gai_strerror(ecode);
+const char * gai_strerror(int ecode) {
+	return __gai_strerror(ecode);
 }
